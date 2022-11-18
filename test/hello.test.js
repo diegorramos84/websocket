df3bff9c0675921aaefde61d26e6899c33af4e59
Diego Ramos <diegorramos84@gmail.com>
2022-11-18 14:40:08 +0000

    add utils to check if the connection is open

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,4 +1,5 @@
 import WebSocket from "ws"
+import { waitForSocketState } from "../utils/webSocketUtils";
 
 describe("Connect to server", () => {
   beforeAll(() => {
@@ -9,7 +10,8 @@ describe("Connect to server", () => {
 
   test("send Hello to client", async () => {
     // create test client
-    const client = new WebSocket('ws://51.141.52.52:1234');
+    const client = new WebSocket('ws://51.141.52.52:1234')
+    await waitForSocketState(client, client.OPEN);
     // send message
     const testMessage = {
       "Name": "Diego"}
@@ -18,10 +20,11 @@ describe("Connect to server", () => {
 
     client.on("message", (data) => {
       responseMessage = data
+      client.close
     })
 
     client.send(JSON.stringify(testMessage))
     console.log(testMessage)
-    expect(responseMessage).toBe(testMessage)
+    expect(responseMessage).toContain('Please answer your first question when all players have joined the room')
   })
 })
---
+++ utils/webSocketUtils.js
@@ -0,0 +1,12 @@
+function waitForSocketState(socket, state) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      if (socket.readyState === state) {
+        resolve();
+      } else {
+        waitForSocketState(socket, state).then(resolve);
+      }
+    }, 5);
+  });
+}
+export {waitForSocketState };
